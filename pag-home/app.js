7bf773c0bc97cdd46ebcac25289e7d96e5e3fd1c
pedroplvs <pedroplvs@gmail.com>
2022-09-23 16:10:35 -0300

    .

--- pag-home/app.js
+++ pag-home/app.js
@@ -2,8 +2,7 @@ function exit (){
     var exit = window.document.getElementsByClassName("exit")
     exit.onclick = confirm ("Tem certeza que deseja sair?")
     if (exit.onclick == true) {
-        window.open("/pag-index/index.html")
-        window.close()
+        window.open("/pag-index/index.html", '_self').window.close()
     } else {
         
     }
