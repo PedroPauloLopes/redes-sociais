25c0ebff7923549837172ae4a9251d1d3d3cd86b
Pedro <109972071+pedroplvs@users.noreply.github.com>
2022-09-23 16:01:28 -0300

    exit ()

---
+++ pag-home/app.js
@@ -0,0 +1,11 @@
+function exit (){
+    var exit = window.document.getElementsByClassName("exit")
+    exit.onclick = confirm ("Tem certeza que deseja sair?")
+    if (exit.onclick == true) {
+        window.open("/pag-index/index.html")
+        window.close()
+    } else {
+        
+    }
+}
+
