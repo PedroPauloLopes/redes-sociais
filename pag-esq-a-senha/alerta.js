a3505743d7944392c8e9d9a24f8f8601c58bae5b
Johnnyk2230 <johnnycds@hotmail.com>
2022-09-24 19:21:54 -0300

    redefinicao de senha
    redefinicao de senha

---
+++ pag-esq-a-senha/alerta.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector ('button');
+
+btn.addEventListener('click', function(){
+    alert("Um email de redefinição de senha foi enviado para para o seu email. Obrigado.");
+});
