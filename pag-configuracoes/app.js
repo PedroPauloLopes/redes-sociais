c023e1c5180734da15eba2726d9165eebb18424e
pedroplvs <pedroplvs@gmail.com>
2022-10-03 16:41:19 -0300

    .

--- pag-configuracoes/app.js
+++ pag-configuracoes/app.js
@@ -1,19 +1,19 @@
-function dark() {
-    var dark = window.document.getElementById("btndark")
-    var div = window.document.getElementById("title")
+// function dark() {
+//     var dark = window.document.getElementById("btndark")
+//     var div = window.document.getElementsByClassName("title")
 
-    document.body.style.backgroundColor = "#050505"
-    document.body.style.color = "FFFFFF"
-    div.setAttribute("id", "title2")
+//     document.body.style.backgroundColor = "#050505"
+//     document.body.style.color = "#FFFFFF"
+//     div.setAttribute('class', 'title2')
      
-}
+// }
 
-function normal() {
-    var dark = window.document.getElementById("btnnormaldark")
-    var div = window.document.getElementById("title2")
+// function normal() {
+//     var dark = window.document.getElementById("btnnormaldark")
+//     var div = window.document.getElementsByClassName("title2")
 
-    document.body.style.backgroundColor = ""
-    document.body.style.color = ""
-    div.setAttribute("id", "title")
+//     document.body.style.backgroundColor = ""
+//     document.body.style.color = ""
+//     div.setAttribute("class", "title")
      
-}
+// }
